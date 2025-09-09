38d69b49b542b359e8d073fc69a5ce70ebfe1ef5
brunodsazevedo <bruno.ds.azevedo@gmail.com>
2025-09-08 23:12:26 -0300

    feat: atualização de configuração eslint 9 para react/react native

---
+++ index.js
@@ -0,0 +1,11 @@
+// Main configuration file that can be used directly
+import reactConfig from './react.js'
+
+export default reactConfig
+
+// Also export named configs for flexibility
+export { default as react } from './react.js'
+
+// You can add more specific configs here, for example:
+// export { default as node } from './node.js'
+// export { default as next } from './next.js'
--- react.js
+++ react.js
@@ -1,65 +1,107 @@
-module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    jest: true,
-  },
-  extends: [
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'standard',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
-    },
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  plugins: [
-    'react',
-    'jsx-a11y',
-    '@typescript-eslint'
-  ],
-  rules: {
-    "react/self-closing-comp": "error",
-    'prettier/prettier': ["error", {
-      'printWidth': 80,
-      'tabWidth': 2,
-      'singleQuote': true,
-      'trailingComma': 'all',
-      'arrowParens': 'always',
-      'semi': false,
-      'endOfLine': 'auto',
-    }],
-    'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off',
-    'jsx-a11y/alt-text': [
-      'warn',
-      {
-        elements: ['img'],
-        img: ['Image'],
+import js from '@eslint/js'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
+import typescript from '@typescript-eslint/eslint-plugin'
+import typescriptParser from '@typescript-eslint/parser'
+import prettier from 'eslint-plugin-prettier'
+import prettierConfig from 'eslint-config-prettier'
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
       },
-    ],
-    'jsx-a11y/aria-props': 'warn',
-    'jsx-a11y/aria-proptypes': 'warn',
-    'jsx-a11y/aria-unsupported-elements': 'warn',
-    'jsx-a11y/role-has-required-aria-props': 'warn',
-    'jsx-a11y/role-supports-aria-props': 'warn',
-    'react/no-unknown-property': 'error',
-  },
-  settings: {
-    react: {
-      version: 'detect',
+      globals: {
+        // Browser globals
+        window: 'readonly',
+        document: 'readonly',
+        console: 'readonly',
+        // Jest globals
+        describe: 'readonly',
+        test: 'readonly',
+        expect: 'readonly',
+        beforeEach: 'readonly',
+        afterEach: 'readonly',
+        beforeAll: 'readonly',
+        afterAll: 'readonly',
+        jest: 'readonly',
+      },
     },
-    'import/parsers': {
-      [require.resolve('@typescript-eslint/parser')]: ['.ts', '.tsx', '.d.ts'],
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'jsx-a11y': jsxA11y,
+      '@typescript-eslint': typescript,
+      prettier,
+    },
+    rules: {
+      // JavaScript recommended rules
+      ...js.configs.recommended.rules,
+      
+      // TypeScript recommended rules
+      ...typescript.configs.recommended.rules,
+      
+      // React recommended rules
+      ...react.configs.recommended.rules,
+      
+      // React Hooks rules
+      ...reactHooks.configs.recommended.rules,
+      
+      // JSX A11y recommended rules
+      ...jsxA11y.configs.recommended.rules,
+      
+      // Prettier rules (should come last to override conflicting rules)
+      ...prettierConfig.rules,
+      
+      // Custom rules
+      'react/self-closing-comp': 'error',
+      'prettier/prettier': [
+        'error',
+        {
+          printWidth: 80,
+          tabWidth: 2,
+          singleQuote: true,
+          trailingComma: 'all',
+          arrowParens: 'always',
+          semi: false,
+          endOfLine: 'auto',
+        },
+      ],
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      'jsx-a11y/alt-text': [
+        'warn',
+        {
+          elements: ['img'],
+          img: ['Image'],
+        },
+      ],
+      'jsx-a11y/aria-props': 'warn',
+      'jsx-a11y/aria-proptypes': 'warn',
+      'jsx-a11y/aria-unsupported-elements': 'warn',
+      'jsx-a11y/role-has-required-aria-props': 'warn',
+      'jsx-a11y/role-supports-aria-props': 'warn',
+      'react/no-unknown-property': 'error',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+      'import/parsers': {
+        '@typescript-eslint/parser': ['.ts', '.tsx', '.d.ts'],
+      },
     },
   },
-  ignorePatterns: [
-    'node_modules'
-  ]
-}
+  {
+    ignores: ['node_modules/**', 'dist/**', 'build/**'],
+  },
+]
